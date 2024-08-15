fc96114e95b09dc70d005aef84013c9b0755caa0
hkw2304 <111471255+hkw2304@users.noreply.github.com>
2024-08-15 14:35:32 +0900

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import {useState, useEffect} from 'react';
+import Layout from './components/layout';
+import Home from './routes/home';
+import Profile from './routes/profile';
+import Login from './routes/login';
+import CreateAccount from './routes/create-account';
+import reset from 'styled-reset';
+import LoadingScreen from './components/loading-screen';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "",
+        element: <Home></Home>
+      },
+      {
+        path: "profile",
+        element: <Profile></Profile>
+      }
+    ],
+  },
+  {
+    path: "login",
+    element: <Login></Login>
+  },
+  {
+    path: "create-account",
+    element: <CreateAccount></CreateAccount>
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body{
+  background-color: black;
+  color: white;
+  font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+  }
+`;
+
+
 
 function App() {
-
+  const [isLoading, setIsLoaing] = useState(true);
+  const init = async () => {
+    setTimeout(() => setIsLoaing(false), 2000);
+  };
+  useEffect(() => {
+    init()
+  }, []);
 
   return (
     <>
-      
+      <GlobalStyles></GlobalStyles>
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   )
 }
---
+++ src/firebase.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAb6YTdDq0xNed3XMZoRPMGkTmy0o0iM3I",
+  authDomain: "twitterproject-65fd8.firebaseapp.com",
+  projectId: "twitterproject-65fd8",
+  storageBucket: "twitterproject-65fd8.appspot.com",
+  messagingSenderId: "945550914117",
+  appId: "1:945550914117:web:7dfaaa927f9e517b96e92c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
